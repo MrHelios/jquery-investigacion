5e26e8faee00b1f3e48f0bc5d896b021540f7e8a
lucho <lhitaliani@gmail.com>
2016-03-03 03:13:26 -0300

    Algunos ejemplos de efectos

--- JQuery_Exp/urls.py
+++ JQuery_Exp/urls.py
@@ -5,5 +5,11 @@ from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
     url(r'^$', views.index, name='index'),
+    url(r'^fade$', views.ejemplos_fade, name='fade'),
+    url(r'^slide$', views.ejemplos_slide, name='slide'),
+    url(r'^animate$', views.ejemplos_animate, name='animate'),
+    url(r'^stop$', views.ejemplos_stop, name='stop'),
+    url(r'^cadena$', views.ejemplos_cadena, name='cadena'),
 ]
--- JQuery_Exp/views.py
+++ JQuery_Exp/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'HTML/index.html')
+
+def ejemplos_fade(request):
+    return render(request, 'HTML/fade.html')
+
+def ejemplos_slide(request):
+    return render(request, 'HTML/slide.html')
+
+def ejemplos_animate(request):
+    return render(request, 'HTML/animate.html')
+
+def ejemplos_stop(request):
+    return render(request, 'HTML/stop.html')
+
+def ejemplos_cadena(request):
+    return render(request, 'HTML/cadena.html')
